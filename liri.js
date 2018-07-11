437b30ae622346e0299e2d4af7aad43e41e08a41
Ayemae <AlyssaAlecci@gamil.com>
2018-07-11 14:55:50 -0400

    some stuff

--- liri.js
+++ liri.js
@@ -3,3 +3,21 @@ require("dotenv").config();
 var keys = require("key");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var requestInput = process.argv[2];
+var requestSubject = process.argv[3];
+
+switch (requestInput) {
+    case "my-tweets":
+
+    break;
+    case "spotify-this-song":
+
+    break;
+    case "movie-this":
+
+    break;
+    case "do-what-it-says":
+
+    break;
+}
