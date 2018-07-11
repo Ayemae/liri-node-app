e7ff182d67f8b84870cf3b75eb24ce222aa05c86
Ayemae <AlyssaAlecci@gamil.com>
2018-07-11 14:59:44 -0400

    okay, more

--- liri.js
+++ liri.js
@@ -7,17 +7,33 @@ var client = new Twitter(keys.twitter);
 var requestInput = process.argv[2];
 var requestSubject = process.argv[3];
 
+function getTweets() {
+
+}
+
+function getSong() {
+
+}
+
+function getMovie() {
+
+}
+
+function getFromRandom() {
+
+}
+
 switch (requestInput) {
     case "my-tweets":
-
+    getTweets();
     break;
     case "spotify-this-song":
-
+    getSong();
     break;
     case "movie-this":
-
+    getMovie();
     break;
     case "do-what-it-says":
-
+    getFromRandom();
     break;
 }
